d8e9acf8be21374c48db908afb50d7235428c9e9
MarcoDAniello <marcod448@gmail.com>
2018-02-04 17:49:02 +0100

    input

---
+++ cerchio.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+
+int r;
+int c;
+int s;
+
+printf("Inserisci il valore del raggio: \n");
+scanf("%d", &r);
+
+
+c = 2*3,14*r;
+s = 3,14*(r*r);
+
+
+printf("La circonferenza misura:  %d\n", c);
+printf("La superficie misura:  %d\n", s);
+
+
+
+return 0;
+}
+
+
+
