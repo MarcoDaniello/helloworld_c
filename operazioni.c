cd0969d53ab2656e1cf41c2625c2643a4a0e3d6f
MarcoDAniello <marcod448@gmail.com>
2018-02-04 17:53:49 +0100

    input

--- operazioni.c
+++ operazioni.c
@@ -11,9 +11,9 @@ int divisione;
 
 
 printf("Inserisci il primo numero ");
-scanf("%d"&a,);
+scanf("%d",&a);
 printf("Inserisci il secondo numero ");
-scanf("%d"&b,);
+scanf("%d",&b);
 
 
 somma = a+b;
@@ -21,10 +21,10 @@ sottrazione = a-b;
 moltiplicazione = a*b;
 divisione = a/b;
 
-printf("La somma vale %d\n", somma );
-printf("La sottrazione vale %d\n", sottrazione );
-printf("La moltiplicazione vale %d\n", moltiplicazione );
-printf("La divisione vale%d\n", divisione);
+printf("La somma vale: %d\n", somma );
+printf("La sottrazione vale: %d\n", sottrazione );
+printf("La moltiplicazione vale: %d\n", moltiplicazione );
+printf("La divisione vale: %d\n", divisione);
 
 
 return 0;
