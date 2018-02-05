5bf30d0049f942e947660b28611fac6d638fc2c7
MarcoDAniello <marcod448@gmail.com>
2018-02-05 16:19:27 +0100

    fix

--- cerchio.c
+++ cerchio.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#define pi 3.14159
 int main()
 {
 
@@ -11,8 +11,8 @@ printf("Inserisci il valore del raggio: \n");
 scanf("%d", &r);
 
 
-c = 2*3,14*r;
-s = 3,14*(r*r);
+c = 2*r*pi;
+s = pi*(r*r);
 
 
 printf("La circonferenza misura:  %d\n", c);
