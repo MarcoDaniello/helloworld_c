9abe941c88f79c1cce75d5c2fed2dddf9cdde588
MarcoDAniello <marcod448@gmail.com>
2018-02-05 16:24:33 +0100

    fix

--- operazioni.c
+++ operazioni.c
@@ -8,7 +8,7 @@ int somma;
 int sottrazione;
 int moltiplicazione;
 int divisione;
-
+int resto;
 
 printf("Inserisci il primo numero ");
 scanf("%d",&a);
@@ -20,11 +20,12 @@ somma = a+b;
 sottrazione = a-b;
 moltiplicazione = a*b;
 divisione = a/b;
-
+resto = a%b;
 printf("La somma vale: %d\n", somma );
 printf("La sottrazione vale: %d\n", sottrazione );
 printf("La moltiplicazione vale: %d\n", moltiplicazione );
 printf("La divisione vale: %d\n", divisione);
+printf("Il resto e': %d\n", resto);
 
 
 return 0;
